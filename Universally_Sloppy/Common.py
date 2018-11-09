cf10cd36fbc3c5212343884353bf08ac37050b64
giannamars <gmarschmann@web.de>
2018-11-09 19:01:53 +0100

    Add files via upload

---
+++ Universally_Sloppy/Common.py
@@ -0,0 +1,9 @@
+model_list = [
+              ('German', 4),
+              ('Ingwersen', 15),
+              ('Mend', 19),
+              ('Osmos', 24),
+              ('PECCAT', 59),
+#              ('PECCATPCA', 12)
+
+             ]
